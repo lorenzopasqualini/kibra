a2373a564b18990c360003f5aca1c07681ecf4ea
lorenzopasqualini <lorenzopasqualini@icloud.com>
2024-07-17 16:53:12 +0200

    maps

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
 import { Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>apple maps api</Text>
+    <View style={styles.container}>
+      <MapView style={styles.map} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map:{
+    width: '100%',
+    height: '100%'
+  }
+});
